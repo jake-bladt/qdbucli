cbd379923bd961b5a6576ae75954a895d1667073
Jake Bladt <jake@jakebladt.com>
2014-03-10 23:49:43 -0400

    added repl app

--- qdbucli/Program.cs
+++ qdbucli/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ReplMvc;
+using ReplMvc.Controllers;
+using ReplMvc.Views;
+
 namespace qdbucli
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var view = new ConsoleView();
+
+            var app = new ReplApplication(view, null);
+            app.Repl();
         }
     }
 }
